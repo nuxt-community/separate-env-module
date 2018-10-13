f140a14da28f5e3188646f7a0bef5e8f51e7d414
Alexander Lichter <manniL@gmx.net>
2018-10-13 17:46:51 +0100

    test: improve test setting with dynamic ports

--- test/module.test.js
+++ test/module.test.js
@@ -1,9 +1,10 @@
 const consola = require('consola')
+const getPort = require('get-port')
 const { Nuxt, Builder } = require('nuxt-edge')
 
-const timeout = 60 * 1000
+jest.setTimeout(30 * 1000)
 
-let nuxt
+let nuxt, port
 
 describe('ssr', () => {
   let log
@@ -17,27 +18,32 @@ describe('ssr', () => {
     nuxt = await setupNuxt(require('./fixture/configs/ssr'))
     const { html } = await nuxt.renderRoute('/')
     expect(html).toMatchSnapshot()
-  }, timeout)
+  })
 
   test('correctly assign env variables from function', async () => {
     const nuxt = await setupNuxt(require('./fixture/configs/ssr-with-function'))
     const { html } = await nuxt.renderRoute('/')
     expect(html).toMatchSnapshot()
-    await nuxt.close()
-  }, timeout)
+  })
 
   test('honor custom build.extend function', async () => {
     nuxt = await setupNuxt(require('./fixture/configs/ssr-with-extend'))
     const { html } = await nuxt.renderRoute('/')
     expect(html).toMatchSnapshot()
-  }, timeout)
+  })
 
   test('error when no object is provided', async () => {
     nuxt = await setupNuxt(require('./fixture/configs/no-object'))
     const expectedMessage = 'Could not assign server env variables. Please provide an object or a function that returns an object!'
     const consolaMessages = log.mock.calls.map(c => c[0].message)
     expect(consolaMessages).toContain(expectedMessage)
-  }, timeout)
+  })
+
+  afterEach(async () => {
+    if (nuxt) {
+      await nuxt.close()
+    }
+  })
 })
 
 describe('csr', () => {
@@ -45,23 +51,27 @@ describe('csr', () => {
     nuxt = await setupNuxt(require('./fixture/configs/csr'))
     const { html } = await renderSpaRoute(nuxt, '/')
     expect(html).toMatchSnapshot()
-  }, timeout)
-})
+  })
 
-afterEach(async () => {
-  await nuxt.close()
+  afterEach(async () => {
+    if (nuxt) {
+      await nuxt.close()
+    }
+  })
 })
 
 const setupNuxt = async (config) => {
   const nuxt = new Nuxt(config)
   await new Builder(nuxt).build()
-  await nuxt.listen(3000)
+  port = await getPort()
+  await nuxt.listen(port)
 
   return nuxt
 }
 
+const url = route => `http://localhost:${port}${route}`
+
 const renderSpaRoute = async (nuxt, _url) => {
-  const url = route => `http://localhost:3000${route}`
   const window = await nuxt.renderAndGetWindow(url(_url))
   const head = window.document.head.innerHTML
   const html = window.document.body.innerHTML
